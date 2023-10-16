52d98004bc8cdd64a9805b4d7e4c46921329437c
putbullet <soulaimanettabaas@gmail.com>
2023-10-16 08:39:35 -0700

    style fix

--- printf_int.c
+++ printf_int.c
@@ -8,36 +8,38 @@
  */
 int printf_int(int n)
 {
-    int count = 0;
-    char buffer[12]; /* Maximum number of digits for a 32-bit integer + sign */
+	int count = 0;
+	char buffer[12];
 
-    if (n < 0)
-    {
-        count += write(1, "-", 1);
-        n = -n;
-    }
+	if (n < 0)
+	{
+		count += write(1, "-", 1);
+		n = -n;
+	}
 
-    if (n == 0)
-    {
-        count += write(1, "0", 1);
-    }
-    else
-    {
-        int i = 0;
-        while (n > 0)
-        {
-            buffer[i] = n % 10 + '0';
-            n /= 10;
-            i++;
-        }
+	if (n == 0)
+	{
+		count += write(1, "0", 1);
+	}
+	else
+	{
 
-        while (i > 0)
-        {
-            i--;
-            count += write(1, &buffer[i], 1);
-        }
-    }
+		int i = 0;
 
-    return (count);
+		while (n > 0)
+		{
+			buffer[i] = n % 10 + '0';
+			n /= 10;
+			i++;
+		}
+
+		while (i > 0)
+		{
+			i--;
+			count += write(1, &buffer[i], 1);
+		}
+	}
+
+	return (count);
 }
 
